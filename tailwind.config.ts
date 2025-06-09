f163695dd39ae802763e3f5af2e3861eeb03b67e
Kashyap Mokariya <mokariyakashyap@gmail.com>
2025-06-09 15:25:38 +0530

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT : '#000',
+          100: "#000319"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
